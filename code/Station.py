f36a5102250fc950055c2f17ab0b3529ec5654d0
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-06-25 11:50:41 +0200

    update

--- code/Station.py
+++ code/Station.py
@@ -1,6 +1,6 @@
 import requests
 import xml.etree.ElementTree as ET
-
+import time
 
 # read in city names from csv
 def readCity(filename):
@@ -16,13 +16,22 @@ def computeEvaNums(cities, base_request_string, authToken):
         'Accept': 'application/xml',
         'Authorization': authToken
     }
+    counter = 0
     for city in cities:
+        if (counter == 18):
+            time.sleep(60)
+            counter = 0
         response = requests.get(base_request_string+city, headers=headers)
-        tree = ET.fromstring(response.text)
-        for station in tree.findall('station'):
-            eva = station.get('eva')
-            # print(station.attrib)
-            print(station.get('name'), eva)
+        counter = counter + 1
+        response_status_code = response.status_code
+        if(response_status_code == 200):
+            tree = ET.fromstring(response.text)
+            for station in tree.findall('station'):
+                eva = station.get('eva')
+                # print(station.attrib)
+                print(station.get('name'), eva)
+        else:
+            print("fail")
 
 # array with all city names
 cities = readCity("..\\misc\\table-1.csv")
