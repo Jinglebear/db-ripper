0d43505e6cd378f70072e9f8887427a1c81daddf
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-06-28 16:16:33 +0200

    update API

--- code/Station.py
+++ code/Station.py
@@ -2,48 +2,65 @@ import requests
 import xml.etree.ElementTree as ET
 import time
 import csv
-# read in city names from csv
-def readCity(filename):
-    cities = []
+import json
+### readCityIDs
+# 
+# read in city IDS from file
+def readCityIDs(filename):
+    cityIDS = []
     with open(filename, "r") as file:
         for line in file:
             city = line.strip().split(';')
+            city_ID = city[0]
             city_name = city[1]
-            cities.append(city_name)
-    return cities
-def computeEvaNums(cities, base_request_string, authToken,resultArr):
+            city_tupel = (city_name,city_ID)
+            cityIDS.append(city_tupel)
+    return cityIDS
+
+
+# array with all city ID's 
+cityIDs = readCityIDs("..\\misc\\table-1.csv")
+
+
+### computeEvaNums
+#
+# @param cities(array containing city names that own railway stations)
+# @param base_request_string (the URL used for this request)
+# @param authToken (the authorization Token used for the DB API)
+# @param resultArr (the array were all railway station names with belonging eva numbers are stored)
+# 
+# collect every eva number for the station in cities (array)
+# write the resulting table into a .csv
+def computeEvaNums(cityIDs, base_request_string, authToken,resultArr):
     headers = {
-        'Accept': 'application/xml',
+        'Accept': 'application/json',
         'Authorization': authToken
     }
     counter = 0
-    for city in cities:
-        if (counter < 18):
+    for city_ID in cityIDs:
+        if (counter < 50):
             #time.sleep(60)
             #counter = 0 # COMMENT THIS LINE OUT FOR TESTING
-            response = requests.get(base_request_string+city, headers=headers)
+            response = requests.get(base_request_string+city_ID[1], headers=headers)
             counter = counter + 1
             response_status_code = response.status_code
             if(response_status_code == 200):
-                tree = ET.fromstring(response.text)
-                for station in tree.findall('station'):
-                    eva = station.get('eva')
-                    # print(station.attrib)
-                    name = station.get('name')
-                    tupel = (name,eva)
-                    resultArr.append(tupel)
-                    print(name + ' ' + eva)
+                data = json.loads(response.text)
+                result_list = data['result']
+                evaNumbers_list = result_list[0]['evaNumbers']
+                evaNumber = evaNumbers_list[0]['number']
+                eva_tupel = (city_ID[0],evaNumber)
+                resultArr.append(eva_tupel)
             else:
                 print("fail")
 
-# array with all city names
-cities = readCity("..\\misc\\table-1.csv")
 
-#test
+
+
 # the base request string for recieving eva numbers
-base_request_string = "https://api.deutschebahn.com/timetables/v1/station/"
+base_request_string = "https://api.deutschebahn.com/stada/v2/stations/"
 resultArr = []
-computeEvaNums(cities,base_request_string,'Bearer 873be58d3db312b4e52a2102e5641c27',resultArr)
+computeEvaNums(cityIDs,base_request_string,'Bearer 873be58d3db312b4e52a2102e5641c27',resultArr)
 
 #time.sleep(120)
 with open("..\\misc\\table-1-result.csv","w") as resultfile:
