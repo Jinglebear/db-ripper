6c04879d08fbad00df85b353615e532c92c2c96b
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-06-28 16:34:57 +0200

    update Station.py (write failures to fail.csv)

--- src/Station.py
+++ src/Station.py
@@ -3,9 +3,9 @@ import xml.etree.ElementTree as ET
 import time
 import csv
 import json
-### readCityIDs
+### readCityIDs and Names
 # 
-# read in city IDS from file
+# read in city IDS and names from file
 def readCityIDs(filename):
     cityIDS = []
     with open(filename, "r") as file:
@@ -24,23 +24,21 @@ cityIDs = readCityIDs("..\\misc\\table-1.csv")
 
 ### computeEvaNums
 #
-# @param cities(array containing city names that own railway stations)
+# @param city(array containing city names that own railway stations)
 # @param base_request_string (the URL used for this request)
 # @param authToken (the authorization Token used for the DB API)
 # @param resultArr (the array were all railway station names with belonging eva numbers are stored)
 # 
 # collect every eva number for the station in cities (array)
 # write the resulting table into a .csv
-def computeEvaNums(cityIDs, base_request_string, authToken,resultArr):
+def computeEvaNums(cityIDs, base_request_string, authToken,resultArr,failArr):
     headers = {
         'Accept': 'application/json',
         'Authorization': authToken
     }
     counter = 0
     for city_ID in cityIDs:
-        if (counter < 50):
-            #time.sleep(60)
-            #counter = 0 # COMMENT THIS LINE OUT FOR TESTING
+        if (counter < 100):
             response = requests.get(base_request_string+city_ID[1], headers=headers)
             counter = counter + 1
             response_status_code = response.status_code
@@ -51,8 +49,14 @@ def computeEvaNums(cityIDs, base_request_string, authToken,resultArr):
                 evaNumber = evaNumbers_list[0]['number']
                 eva_tupel = (city_ID[0],evaNumber)
                 resultArr.append(eva_tupel)
+                print(eva_tupel,flush=True)
             else:
                 print("fail")
+                failArr.append(city_ID)
+        else:
+            print('sleeping...')
+            time.sleep(60)
+            counter = 0 
 
 
 
@@ -60,15 +64,18 @@ def computeEvaNums(cityIDs, base_request_string, authToken,resultArr):
 # the base request string for recieving eva numbers
 base_request_string = "https://api.deutschebahn.com/stada/v2/stations/"
 resultArr = []
-computeEvaNums(cityIDs,base_request_string,'Bearer 873be58d3db312b4e52a2102e5641c27',resultArr)
+failArr = []
+computeEvaNums(cityIDs,base_request_string,'Bearer 873be58d3db312b4e52a2102e5641c27',resultArr,failArr)
 
 #time.sleep(120)
 with open("..\\misc\\table-1-result.csv","w") as resultfile:
     writer = csv.writer(resultfile)
     for result in resultArr:
         writer.writerow(result)
-
-
+with open("..\\misc\\table-1-fail.csv","w") as failfile:
+    writer = csv.writer(failfile)
+    for fail in failArr:
+        writer.writerow(fail)
 
 
 
