06e88572638ef75b69a8a2259545e82947c3bfd7
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-07-02 13:09:54 +0200

    update Station script

--- src/Station.py
+++ src/Station.py
@@ -1,6 +1,12 @@
 import time
 import requests
 import csv
+import sys
+from itertools import groupby
+
+def all_equal(counterArr):
+    g = groupby(counterArr)
+    return next(g,True) and not next(g,False)
 
 
 def readStation(filename):
@@ -31,65 +37,61 @@ def getEvaNum(station, base_request_string, header, resultArr, failArr):
         evaNumber = response['result'][0]['evaNumbers'][0]['number']
         eva_triplet = (station[0], evaNumber, station[2])
         resultArr.append(eva_triplet)
-        print(eva_triplet, flush=True)  # debug
+        #print(eva_triplet, flush=True)  # debug
     else:
         print("ERROR: "+str(response_status_code), flush=True)  # debug
         failArr.append(station)
 
 
-def computeEvaNums(stations, base_request_string, headers, resultArr, failArr):
-    counter1 = 0
-    counter2 = 0
-    counter3 = 0
-    counter4 = 0
+def computeEvaNums(stations, base_request_string, headers, resultArr, failArr, counterArr):
+    control = 0
     for station in stations:
+        
+        control +=1
+        print(control,flush=True)
         try:
-            if (counter1 < 100):
-                counter1 += 1
-                getEvaNum(station=station, base_request_string=base_request_string,
-                          header=headers[0], resultArr=resultArr, failArr=failArr)
-            elif(counter2 < 100):
-                counter2 += 1
-                getEvaNum(station=station, base_request_string=base_request_string,
-                          header=headers[1], resultArr=resultArr, failArr=failArr)
-            elif(counter3 < 100):
-                counter3 += 1
-                getEvaNum(station=station, base_request_string=base_request_string,
-                          header=headers[2], resultArr=resultArr, failArr=failArr)
-            elif(counter4 < 100):
-                counter4 += 1
-                getEvaNum(station=station, base_request_string=base_request_string,
-                          header=headers[3], resultArr=resultArr, failArr=failArr)
-            if(counter1 == 100 and counter2 == 100 and counter3 == 100 and counter4 == 100):
-                time.sleep(60)
-                counter1 = 0
-                counter2 = 0
-                counter3 = 0
-                counter4 = 0
+            for i in range(len(counterArr)):
+                if(counterArr[i] < 100):
+                    counterArr[i] +=1
+                    getEvaNum(station=station, base_request_string=base_request_string,
+                    header=headers[i], resultArr=resultArr, failArr=failArr)
+                    break
+            sleep = all_equal(counterArr=counterArr)
+            if(sleep):
+                print("<<<<<<<<<<<<<<SLEEPING>>>>>>>>>>>>>>>>",flush=True)
+                time.sleep(61)
+                for j in range(len(counterArr)):
+                    counterArr[j] = 0
         except IndexError:
             print("ERROR: IndexError", flush=True)  # debug
             failArr.append(station)
             failArr.append("(IndexError)")
         except:
-            print("ERROR: UnkownError", flush=True)  # debug
+            e = sys.exc_info()
+            print(e)
 
 
 base_request_string = "https://api.deutschebahn.com/stada/v2/stations/"
 resultArr = []
 failArr = []
 tokenArr = ['Bearer c63151b12eb8b7a48ef869a72c77c15f', 'Bearer 0548da9b09541d93c01c2685f5e8a611',
-            'Bearer 9813ceeae64e273dca14d615405395d7', 'Bearer 194291fed31c8daf95c8d45f22254399']
-headers_arr = [{'Accept': 'application/json',
-               'Authorization': tokenArr[0]}, {'Accept': 'application/json',
-               'Authorization': tokenArr[1]}, {'Accept': 'application/json',
-               'Authorization': tokenArr[2]}, {'Accept': 'application/json',
-               'Authorization': tokenArr[3]}]
-computeEvaNums(stations, base_request_string, headers_arr, resultArr, failArr)
-with open("..\\misc\\table-1-result.csv", "w", encoding='utf-8') as resultfile:
+            'Bearer 9813ceeae64e273dca14d615405395d7', 'Bearer 194291fed31c8daf95c8d45f22254399',
+            'Bearer fca48279e7030608b9799ab4e84c8975', 'Bearer b4a1b8aa689f4b1f2666af53a1b592ab',
+            'Bearer 8312dec31d1808720aaadd9334accb24', 'Bearer eed6fa376f86dce95beb400315616e58']
+headers_arr =[]
+for token in tokenArr:
+    header = {'Accept':'application/json', 'Authorization' : token}
+    headers_arr.append(header)
+counterArr =[]
+for i in range(8):
+    counterArr.append(0)
+
+computeEvaNums(stations, base_request_string, headers_arr, resultArr, failArr,counterArr=counterArr)
+with open("..\\misc\\table-1-result.csv", "w", newline='', encoding='utf-8') as resultfile:
     writer = csv.writer(resultfile)
     for result in resultArr:
         writer.writerow(result)
-with open("..\\misc\\table-1-fail.csv", "w", encoding='utf-8') as failfile:
+with open("..\\misc\\table-1-fail.csv", "w", newline='', encoding='utf-8') as failfile:
     writer = csv.writer(failfile)
     for fail in failArr:
         writer.writerow(fail)
