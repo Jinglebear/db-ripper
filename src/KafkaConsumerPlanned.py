aa62e0b0aed2623b73b7982121134588c2a471ca
Jackafive753 <FiredevilJacksonNrFive@gmx.de>
2021-07-09 13:09:58 +0200

    fix(Consumer): catch empty timetable

--- src/KafkaConsumerPlanned.py
+++ src/KafkaConsumerPlanned.py
@@ -1,3 +1,4 @@
+from os import EX_CANTCREAT
 import threading
 from kafka import KafkaConsumer
 import xml.etree.ElementTree as ET
@@ -27,34 +28,36 @@ def factorize_message(xmlString):
     root = ET.fromstring(xmlString)
 
     # extract trainStation out of xml
-    trainStation = root.attrib['station']
+    try:
+        trainStation = root.attrib['station']
 
-    # child of timetable are event with tag "s"
-    for s in root:
-        # save values in trainInformation
-        trainInformation = {}
-        trainInformation['station'] = trainStation
+        # child of timetable are event with tag "s"
+        for s in root:
+            # save values in trainInformation
+            trainInformation = {}
+            trainInformation['station'] = trainStation
 
-        trainInformation['event'] = 'timetable'
-        trainInformation['id'] = s.attrib['id']
-        tripLabel = s.find('tl')
-        trainInformation['trainType'] = tripLabel.attrib.get('f')
-        trainInformation['trainCategory'] = tripLabel.attrib.get('c')
+            trainInformation['event'] = 'timetable'
+            trainInformation['id'] = s.attrib['id']
+            tripLabel = s.find('tl')
+            trainInformation['trainType'] = tripLabel.attrib.get('f')
+            trainInformation['trainCategory'] = tripLabel.attrib.get('c')
 
-        # extract arriveTime if arrive event exist
-        arrive = s.find('ar')
-        if arrive != None:
-            trainInformation['arTime'] = arrive.attrib.get('pt')
+            # extract arriveTime if arrive event exist
+            arrive = s.find('ar')
+            if arrive != None:
+                trainInformation['arTime'] = arrive.attrib.get('pt')
 
-        # extract departureTime if departure event exist
-        departure = s.find('dp')
-        if departure != None:
-            trainInformation['dpTime'] = departure.attrib.get('pt')
+            # extract departureTime if departure event exist
+            departure = s.find('dp')
+            if departure != None:
+                trainInformation['dpTime'] = departure.attrib.get('pt')
 
-        # transform dictinary to json and save on elasticsearch
-        json_object = json.dumps(trainInformation)
-        save_on_elasticsearch(json_object, trainInformation['id'])
-
+            # transform dictinary to json and save on elasticsearch
+            json_object = json.dumps(trainInformation)
+            save_on_elasticsearch(json_object, trainInformation['id'])
+    except:
+        print(xmlString)
 
 consumer = KafkaConsumer(Utils.topicForPlannedTimetables, group_id='db_ripper' , bootstrap_servers=Utils.bootstrap_servers)
 
