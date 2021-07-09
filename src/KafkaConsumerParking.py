fcddcc8b9b0f827245f4019fa98fe1f43b3f8c7b
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-07-09 10:55:50 +0200

    upload KafkaConsumerParking.py

---
+++ src/KafkaConsumerParking.py
@@ -0,0 +1,48 @@
+from kafka import KafkaConsumer
+from Utility import Utils
+import json
+import requests
+
+
+# save incoming json on elasticsearch
+def save_on_elasticsearch(parkingSpaceJson, id):
+    # connect to elasticsearch with default config
+    _es = Utils.connect_elasticsearch()
+    if (_es == None):
+        return
+    
+    # create index if not exists with defaultname
+    Utils.create_index(_es)
+
+    try:
+        _es.index(Utils.esIndex, body=parkingSpaceJson)
+    except Exception as e:
+        print('Error in indexing data')
+        print(str(e))
+
+
+# fetch space data, create json and save on elasticsearch
+def extract_space_data(response):
+    response = response.json()
+
+    allocations = response['allocations']
+
+    for allocation in allocations:
+        parkingInformation = {}
+
+        if(len(allocation["allocation"]) == 6):
+            allocation_id = allocation['space']['id']
+            allocation_station_name = allocation['space']['station']['name']
+            allocation_category = allocation['allocation']['category']
+            
+            parkingInformation['spaceID'] = allocation_id
+            parkingInformation['stationName'] = allocation_station_name
+            parkingInformation['parkingCategory'] = allocation_category
+            
+            save_on_elasticsearch(json.dumps(parkingInformation,indent=3))
+
+consumer = KafkaConsumer(Utils.topicParkingTimetables, group_id='db_ripper',bootstrap_servers=Utils.bootstrap_servers)
+
+for message in consumer:
+    extract_space_data(message)
+    
