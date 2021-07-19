854e4df475b63061d9bf777cfdcf92d03fcf3917
Jinglebear <jinglebearbeowulf@gmail.com>
2021-07-19 17:13:54 +0200

    updated ConsumerParking.sh

--- src/sh_scripts/ConsumerParking.sh
+++ src/sh_scripts/ConsumerParking.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Log message
-printf 'Starting KafkaKonsumerParking...\n'
+printf 'Starting KafkaConsumerParking...\n'
 
-python3 /home/nils/db-ripper/src/KafkaConsumerParking.py 
+python3 /home/nils/db-ripper/src/KafkaConsumerParking.py >> /home/nils/db-ripper/misc/logs/KafkaConsumerParking.log 2>>/home/nils/db-ripper/misc/err_logs/ErrLogConsumerParking.log 
