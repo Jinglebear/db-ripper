d9d688a4e272d27fccd7dde639901e94b8345e50
Jinglebear <jinglebearbeowulf@gmail.com>
2021-07-19 17:46:37 +0200

    update

--- src/sh_scripts/ProducerParking.sh
+++ src/sh_scripts/ProducerParking.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+#Log message
+printf 'Starting KafkaProducerParking...\n'
+
+python3 /home/"$USER"/db-ripper/src/KafkaProducerParking.py >> /home/"$USER"/db-ripper/misc/logs/KafkaProducerParking.log 2>>/home/"$USER"/db-ripper/misc/err_logs/ErrLogProducerParking.log
