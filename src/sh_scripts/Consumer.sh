5b1bd50019a4e8a426dabe1f8858bb14389a98df
Jackafive753 <you@example.com>
2021-07-23 11:19:59 +0200

    merge parking consumer in consumer

--- src/sh_scripts/Consumer.sh
+++ src/sh_scripts/Consumer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-path=/home/johannes/db-ripper
+user=johannes
+path=/home/"$user"/db-ripper
 
 python3 "$path"/src/KafkaConsumerPlanned.py >> "$path"/misc/logs/logKafkaConsumerPlanned.log 2>> "$path"/misc/err_logs/ErrLogKafkaConsumerPlanned.log&
 python3 "$path"/src/KafkaConsumerChanges.py >> "$path"/misc/logs/logKafkaConsumerChanges.log 2>> "$path"/misc/err_logs/ErrLogKafkaConsumerChanges.log&
+python3 "$path"/src/KafkaConsumerParking.py >> "$path"/misc/logs/logKafkaConsumerParking.log 2>> "$path"/misc/err_logs/ErrLogConsumerParking.log&
--- src/sh_scripts/ConsumerParking.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Log message
-printf 'Starting KafkaConsumerParking...\n'
-
-python3 /home/"$USER"/db-ripper/src/KafkaConsumerParking.py >> /home/"$USER"/db-ripper/misc/logs/KafkaConsumerParking.log 2>>/home/"$USER"/db-ripper/misc/err_logs/ErrLogConsumerParking.log 
