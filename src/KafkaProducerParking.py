fdfbc72d635a3dba3ce27c0be0420405ad839324
Jinglebear <jinglebearbeowulf@gmail.com>
2021-07-23 09:19:45 +0200

    feat(Parking): add Error/Exception handling

--- src/KafkaProducerParking.py
+++ src/KafkaProducerParking.py
@@ -1,14 +1,12 @@
+import sys
 from datetime import datetime
-import time
-from kafka import KafkaProducer, producer
-from utility import Utils
-import requests
-#constants
-request_string = "https://api.deutschebahn.com/bahnpark/v1/spaces/occupancies" # /spaces/{id}/occupancies
-header = {
-    'Accept': 'application/json;charset=utf-8',
-    'Authorization': 'Bearer eed6fa376f86dce95beb400315616e58'
-}
+try:
+    import time
+    from kafka import KafkaProducer, producer
+    from utility import Utils
+    import requests
+except Exception as e:
+    print("#",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaProducerParking: Exception by import",e,file=sys.stderr)
 # callback of kafka if send successfull
 def send_on_success(record_metadata):
     print('topic:',record_metadata.topic,'partition:',record_metadata.partition)
@@ -18,20 +16,26 @@ def process_parking_IDs(request_string, header):
     #create producer
     producer=KafkaProducer(bootstrap_servers=Utils.bootstrap_servers)
     try:
+        #api request
         response = requests.get(request_string,headers=header)
         if(response.status_code == 200):
             producer.send(topic=Utils.topicParkingTimetables,value=response.content).add_callback(send_on_success)
+        else:
+            print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaProducerParking: request fail with code", response.status_code, file=sys.stderr)
     except Exception as e:
-        print(e)
+        print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaProducerParking: request fail", e, file=sys.stderr)
+    # wait until every producer send his data
     producer.flush()
 
 #============================
 #Main
-# no loop --> cron job
-#while True: 
-    #get some parking data
+try:
+#constants
+    request_string = "https://api.deutschebahn.com/bahnpark/v1/spaces/occupancies" # /spaces/{id}/occupancies
+    header = {'Accept': 'application/json;charset=utf-8','Authorization': 'Bearer eed6fa376f86dce95beb400315616e58'}
+    #function call
     process_parking_IDs(request_string=request_string,header=header)
-    #get parking data every 30 minutes 
-    #adjust this
-#    time.sleep(1800)
+except Exception as e:
+    print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaProducerParking: Exception in main", e, file=sys.stderr)
+
 
