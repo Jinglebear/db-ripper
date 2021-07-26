ce303a35c68a6d797cc0149aca0197ea1b101fd7
Jackafive753 <you@example.com>
2021-07-26 18:21:01 +0200

    feat(shell): do in background

--- src/sh_scripts/ProducerHourly.sh
+++ src/sh_scripts/ProducerHourly.sh
@@ -3,4 +3,4 @@
 user=johannes
 path=/home/"$user"/db-ripper
 
-python3 "$path"/src/KafkaProducerPlanned.py >> "$path"/misc/logs/logKafkaProducerPlanned.log 2>> "$path"/misc/err_logs/ErrLogKafkaProducerPlanned.log
+python3 "$path"/src/KafkaProducerPlanned.py >> "$path"/misc/logs/logKafkaProducerPlanned.log 2>> "$path"/misc/err_logs/ErrLogKafkaProducerPlanned.log&
