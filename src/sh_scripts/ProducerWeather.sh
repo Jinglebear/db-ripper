35abc47b574ac754ea56d929a427765fe7484a0d
Jackafive753 <you@example.com>
2021-08-17 10:21:59 +0200

    delete not needed script

--- src/sh_scripts/ProducerWeather.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printf 'Starting KafkaProducerWeather..\n'
-
-path=/home/"$USER"/db-ripper
-
-python3 $path/src/KafkaProducerWeather.py >> $path/misc/logs/logKafkaProducerWeather.log 2>> $path/misc/err_logs/ErrLogKafkaProducerWeather.log
