b1e64b2c3ed27c9120382beea47a7f95a08efce2
Jackafive753 <you@example.com>
2021-08-17 11:20:56 +0200

    fix kafka weather

--- src/KafkaConsumerWeather.py
+++ src/KafkaConsumerWeather.py
@@ -1,27 +1,19 @@
-import sys
-from datetime import datetime, timedelta
-import time
-from kafka import consumer
-
-
+from utility import Utils
 
 try:
-    import threading
     from kafka import KafkaConsumer
-    import xml.etree.ElementTree as ET
+    from datetime import datetime
+    from kafka import consumer
     import json
-    from utility import Utils
 except Exception as e:
-    print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaConsumerWeather: Exception by import", e)
+    Utils.print_error("KafkaConsumerWeather", "Error while import", e)
 
 # save incoming json on elasticsearch
-def save_on_elasticsearch(weatherJson):
-    print(json.loads(weatherJson))
+def save_on_elasticsearch(weather_json):
     # connect to elasticsearch with default config
     _es = Utils.connect_elasticsearch()
-    time.sleep(2)
     if _es == None:
-        print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaConsumerWeather: Can't connect to elasticsearch")
+        Utils.print_error("KafkaConsumerWeather", "Can't connect to elasticsearch")
         return
 
     # create index if not exists with defaultname
@@ -29,81 +21,55 @@ def save_on_elasticsearch(weatherJson):
 
     # store json on elasticsearch
     try:
-        _es.index(Utils.es_default_index, body=weatherJson)
-        print("Success!")
+        _es.index(Utils.es_default_index, body=weather_json)
         # print(response)
     except Exception as e:
-        print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaConsumerWeather: Error while indexing data.", e)
+        Utils.print_error("KafkaConsumerWeather", "Error while indexing data", e)
         
 
 
 # factorize incoming message in a thread
 # create json and give them to save_on_elasticsearch
-def factorize_message(kafkaJsonWeatherData):
-    
+def factorize_message(kafka_json_weather_data): 
     try:
-
-        # print(type(kafkaJsonWeatherData))
-        
         # save values in weatherInformation
-        weatherInformation = {}
+        weather_information = {}
 
-        weatherInformation['cityname']= kafkaJsonWeatherData['name']
+        weather_information['city']= kafka_json_weather_data['name']
 
-        coord= [kafkaJsonWeatherData['coord']['lon'],kafkaJsonWeatherData['coord']['lat']]
+        coord= [kafka_json_weather_data['coord']['lon'], kafka_json_weather_data['coord']['lat']]
 
-        weatherInformation['coordinates']=coord
+        weather_information['location']=coord
         
-        # weatherInformation['weatherDescription']= kafkaJsonWeatherData['weather']['main']
+        weather_information['weatherDescription']= kafka_json_weather_data['weather'][0]['main']
 
-        # weatherInformation['weatherSpecificDiscription']=kafkaJsonWeatherData['weather']['description']
+        weather_information['weatherSpecificDescription'] = kafka_json_weather_data['weather'][0]['description']
 
-        # weatherInformation['temperature']=kafkaJsonWeatherData['main']['temp']
+        temp_in_kelvin = kafka_json_weather_data['main']['temp']
+        weather_information['temperature'] = round(temp_in_kelvin - 273.15, 2)
 
-        # weatherInformation['temperature_Min']=kafkaJsonWeatherData['main']['temp_min']
+        weather_information['windSpeed']= kafka_json_weather_data['wind']['speed']
 
-        # weatherInformation['temperature_Max']=kafkaJsonWeatherData['main']['temp_max']
+        weather_information['event']='weather'
 
-        # weatherInformation['wind_Speed']= kafkaJsonWeatherData['wind']['speed']
-
-        weatherInformation['event']='weather'
-
-        currentDT = datetime.now()
-        currentDT_formated = currentDT.strftime("%Y-%m-%dT%H:%M:%S")
-        print(currentDT_formated)
         #add formatted timestamp to weatherInformation JSON Object
-        weatherInformation['timestamp'] = currentDT_formated
+        current_dt = datetime.now()
+        weather_information['timestamp'] = current_dt.strftime(Utils.timestamp_format)
 
         # transform dictinary to json and save on elasticsearch
-        json_object = json.dumps(weatherInformation,indent=4)
-        print(json_object)
-        # print(json_object)
-        save_on_elasticsearch(json_object)# nach Wetterid ablegen oder CityName
+        json_object = json.dumps(weather_information,indent=4)
+
+        save_on_elasticsearch(json_object)
     except Exception as e:
-        print(e)
-        # print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"KafkaConsumerWeather: Error in an event",e, file=sys.stderr)
+        Utils.print_error("KafkaConsumerWeather", "Error while processing json from kafka", e)
 
 ## Work
-print("#", datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "KafkaConsumerWeather: start consumer")
-# consumer = KafkaConsumer(Utils.topicWeather,group_id='db_ripper', bootstrap_servers=Utils.bootstrap_servers)
-consumer=KafkaConsumer('weather',group_id='db-ripper',bootstrap_servers=['localhost:29092'])
+Utils.print_log("KafkaConsumerWeather", "start consumer")
+consumer=KafkaConsumer(Utils.topic_weather, group_id='db-ripper', bootstrap_servers=Utils.bootstrap_servers)
 print(consumer.bootstrap_connected())
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    # print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-    #                                       message.offset, message.key,
-    #                                       message.value))
     messageValue = message.value
     messageValueAsString = messageValue.decode('utf-8')
     factorize_message(json.loads(messageValueAsString))
-# print("rndmom")
-# for message in consumer:
-#     messageValue = message.value
-#     messageValueAsString = messageValue.decode('utf-8')
-#     # thread = threading.Thread(target=factorize_message, args=(json.loads(messageValueAsString),))
-#     # thread.start()
-#     factorize_message(messageValueAsString)
-
-# for message in consumer:
-#     print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
