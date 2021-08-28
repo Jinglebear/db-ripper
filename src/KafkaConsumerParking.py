f341802fbc679defa944d64b22a002aa05e4e8b3
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-08-28 14:05:15 +0200

    added comments, cleaned code

--- src/KafkaConsumerParking.py
+++ src/KafkaConsumerParking.py
@@ -1,4 +1,5 @@
 from utility import Utils
+import csv
 
 try:
     from kafka import KafkaConsumer
@@ -25,7 +26,6 @@ def save_on_elasticsearch(parking_space_json, id):
     except Exception as e:
         Utils.print_error("KafkaConsumerParking: Error while indexing data" , e)
 
-import csv
 
 # fetch space data, create json and save on elasticsearch
 def extract_space_data(response):
@@ -38,6 +38,7 @@ def extract_space_data(response):
 
             if(len(allocation["allocation"]) == 6):
                 allocation_id = allocation['space']['id']
+               
                 #trainstation name
                 allocation_station_name = allocation['space']['station']['name']
                 allocation_category = allocation['allocation']['category']
@@ -47,24 +48,29 @@ def extract_space_data(response):
                 parking_information['stationName'] = allocation_station_name
                 parking_information['parkingCategory'] = allocation_category
                 parking_information['event'] = 'parking'
+                
                 ##array consisting of long and lat
                 city_info = Utils.get_city_info(allocation_station_name)
+                
                 parking_information['location'] = city_info.get('location')
                 parking_information['city'] = city_info.get('cityname')
+                
                 #create timestamp for elastic search
                 current_dt = datetime.now()
                 current_dt_formated = current_dt.strftime("%Y-%m-%dT%H:%M:%S")
+                
                 #add formatted timestamp to parkingInformation JSON Object
                 parking_information['timestamp'] = current_dt_formated
+                
                 #write JSON object on elasticsearch
-                parking_information_as_json = json.dumps(parking_information,indent=5)
-                print(parking_information_as_json)
                 save_on_elasticsearch(json.dumps(parking_information,indent=5),id=allocation_id)
             else:
                 continue
         except Exception as e:
             Utils.print_error("KafkaConsumerParking : Error while extract_space_data:", e)
-            
+
+## Work
+Utils.print_log("KafkaConsumerParking", "start consumer (parking)")       
 consumer = KafkaConsumer(Utils.topic_parking, group_id='db_ripper',bootstrap_servers=Utils.bootstrap_servers)
 
 for message in consumer:
