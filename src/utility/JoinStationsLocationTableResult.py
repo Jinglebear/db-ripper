c0d913d9d42aaa2bd00f215e7ec011efe2b27bb2
Jinglebear <46826783+Jinglebear@users.noreply.github.com>
2021-08-28 14:16:33 +0200

    cleaned up code, added comments

--- src/utility/JoinStationsLocationTableResult.py
+++ src/utility/JoinStationsLocationTableResult.py
@@ -1,5 +1,16 @@
 import csv
-def readEvaTable(filename):
+
+# ====================================================================
+# **Description**
+# This script takes the ~1115 train stations containing :
+# --> train station name 
+# --> evaluation number ( train station identifier in db-api)
+# --> train station category (1-4, 1 means biggest train station)
+# --> name of the city the train station is placed in
+# and enriches it with the geoJSON (Point) by joining it with location table
+
+# read 1115 train stations
+def read_eva_table(filename):
     stations=[]
     with open(file=filename, encoding='utf-8') as file:
         reader = csv.reader(file)
@@ -12,10 +23,12 @@ def readEvaTable(filename):
             #add station to the array of stations
             stations.append(station)
     return stations
-stationsEva = readEvaTable("..\\..\\misc\\table-2-sorted(category4).csv")
 
+# function call
+stationsEva = read_eva_table("..\\..\\misc\\table-2-sorted(category4).csv")
 
-def readLocationTable(filename):
+# read location table
+def read_location_table(filename):
     stations=[]
     with open(file=filename, encoding='utf-8') as file:
         reader = csv.reader(file)
@@ -28,9 +41,13 @@ def readLocationTable(filename):
             #add station to the array of stations
             stations.append(station)
     return stations
-stationsLocation = readLocationTable("..\\..\\misc\\table_stations_location_final.csv")
+
+#function call
+stationsLocation = read_location_table("..\\..\\misc\\table_stations_location_final.csv")
+# joined data array
 stationsJoined = []
 
+# join data
 for stationEva in stationsEva:
     for stationLocation in stationsLocation:
         if(stationEva[0] == stationLocation[0]):
@@ -46,11 +63,7 @@ for station in stationsJoined:
     else:
         stationsJoinedNoDpS.append(station)
 
-####
 #write result in new file joinedTables.csv
-
-
-
 with open("..\\..\\misc\\joined_tables.csv","w",newline='',encoding='utf-8')as outputfile:
     writer = csv.writer(outputfile)
     for station in stationsJoinedNoDpS:
